642527bf8dca3dbb0769481ea6a19e65ceea2554
German Shlyaiger <germanshlyaiger14@gmail.com>
2023-06-20 16:24:53 +0600

    Added main directories and additional packages

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "root": true,
+    "parser": "babel-eslint",
+    "extends": [
+      "airbnb"
+    ],
+    "rules":{
+      "no-unused-vars": "off",
+      "quotes": ["single"],
+    },
+    "env": {
+      "browser": true,
+      "node": true,
+      "jest": true
+    },
+  }
+  
