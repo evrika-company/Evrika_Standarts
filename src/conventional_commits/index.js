23efdf4a2654595f28489ace759fc046d478c865
German Shlyaiger <germanshlyaiger14@gmail.com>
2023-06-22 14:18:54 +0600

    Update commit types and create specific directories

---
+++ src/conventional_commits/index.js
@@ -0,0 +1,95 @@
+const getCommitTypesToCommitizen = function() {
+
+};
+
+const getCommitTypesToChangelock = function() {
+
+};
+
+
+const commitTypes = [
+  {
+    value: 'build',
+    name: 'build:      Изменения процесса сборки',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'package',
+    name: 'package:      Добавление внешних зависимостей',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'ci',
+    name: '',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'docs',
+    name: '',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'feat',
+    name: '',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'fix',
+    name: '',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'perf',
+    name: '',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'refactor',
+    name: '',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'revert',
+    name: '',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'style',
+    name: 'style:      Правки по стилю кода и линтированию',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'test',
+    name: 'test:      Добавление тестов или изменение процесса тестирования',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'custom',
+    name: 'custom:      Изменения имеющие специфичную область действия',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'config',
+    name: 'config:      Изменения в конфигурационных файлах или параметрах',
+    section: '',
+    hidden: '',
+  },
+  {
+    value: 'security',
+    name:'security:      Исправление уязвимостей или улучшение безопасности',
+    section: '',
+    hidden: '',
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const commitlint = require('./commitizen_commitlint/index');
+const changelog = require('./changelog_customize/index');
 const customize = require('./commitizen_customize/index');
+const commitlint = require('./commitlint_customize/index');
 
 module.exports = {
   customize,
+  changelog,
   commitlint,
 };
