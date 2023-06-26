05924efdcfb52b2d8aff4c5fbddc3e41016c336e
German Shlyaiger <germanshlyaiger14@gmail.com>
2023-06-26 18:43:50 +0600

    change(code): update code data

--- src/changelog_customize/index.js
+++ src/changelog_customize/index.js
@@ -4,31 +4,20 @@ const conventionalCommits = require('../conventional_commits/index');
 
 const getCommitTypesToChangelock = function(types) {
     return types.map(function(element) {
-        //console.log(element,33)
-        let typeCommit = {
+
+      let typeCommit = {
           "type": element.value,
         };
 
         element.section ? typeCommit.section = element.section : typeCommit.hidden = element.hidden;
-        console.log(typeCommit,333)
         return typeCommit
     })
 };
 
 module.exports = {
-      "name": "conventionalcommits",
-      "types": [
-        ...getCommitTypesToChangelock(conventionalCommits.commitTypes),
-        {}
-      ],
+  "name": "conventionalcommits",
+  "types": [
+    ...getCommitTypesToChangelock(conventionalCommits.commitTypes),
+    {}
+  ],
 }
-
-// console.log(999,{
-//     "infile": "CHANGELOG.md",
-//     "header": "Журнал изменений",
-//     "preset": {
-//       "name": "conventionalcommits",
-//       "types": [
-//         ...getCommitTypesToChangelock(conventionalCommits.commitTypes)
-//       ],
-//     }})
