7ec4ffbdfef8df99bff828fbb199330f4730a64d
German Shlyaiger <germanshlyaiger14@gmail.com>
2023-06-26 15:59:40 +0600

    feat(style): работы по форматированию
    поменяли некоторые поля и форматировали большинство файлов конфигурации

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const changelog = require('./changelog_customize/index');
 const customize = require('./commitizen_customize/index');
 const commitlint = require('./commitlint_customize/index');
+const conventional = require('./conventional_commits/index');
 
 module.exports = {
   customize,
   changelog,
   commitlint,
+  conventional,
 };
