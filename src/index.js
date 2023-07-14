343958c8dea7a9bc7be252ecee21b19f24061847
German Shlyaiger <germanshlyaiger14@gmail.com>
2023-07-14 12:23:01 +0600

    fix(components): :bug: добавление коментария на точку входа (тест)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// точка входа для экспорта конфигурации
+
 const changelog = require('./changelog_customize/index');
 const customize = require('./commitizen_customize/index');
 const commitlint = require('./commitlint_customize/index');
